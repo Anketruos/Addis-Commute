db0e47e0664af43e347a882feef062a3a8f7856c
Barkotfrew <141666169+Barkotfrew@users.noreply.github.com>
2025-12-03 00:28:00 +0300

    Add FareCalculator class with basic fare calculation structure

---
+++ src/com/addiscommute/FareCalculator.java
@@ -0,0 +1,12 @@
+package com.addiscommute;
+
+public class FareCalculator {
+    private double baseFare;
+    private double perKmRate;
+    private double lastFare;
+
+
+    public double calculateFare(BusRoute route, BusStop startStop, BusStop endStop) { return 0.0; }
+    public void displayFare(double fare) {  }
+    public double getLastFare() { }
+}
